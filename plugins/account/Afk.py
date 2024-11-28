df0fb29f08640000d52151f661fe8de5163e95d7
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2024-11-28 19:22:04 +0300

    Update Afk.py

--- plugins/account/Afk.py
+++ plugins/account/Afk.py
@@ -5,7 +5,6 @@ from telethon.tl import functions, types
 
 from .. import JmdB, jmubot, Tepthon_cmd
 
-from Tepthon.Config import Config
 from Tepthon.core.managers import edit_delete, edit_or_reply
 from . import BOTLOG, BOTLOG_CHATID
 
